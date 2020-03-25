db3f5a0064908cb3e0c89be739e9c870ba111374
richardjlv <richardjunio39@gmail.com>
2020-03-25 16:05:44 -0300

    Creating SessionController

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,17 @@ import { Router } from 'express';
 
 import IncidentController from './controllers/IncidentController';
 import OngController from './controllers/OngController';
+import ProfileController from './controllers/ProfileController';
+import SessionController from './controllers/SessionController';
 
 const routes = new Router();
 
+routes.post('/sessions', SessionController.store);
+
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.store);
+
+routes.get('/profile', ProfileController.index);
 
 routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.store);
