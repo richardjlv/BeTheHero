022e0b17edd39cab66f7247af7d43480d1435a51
richardjlv <richardjunio39@gmail.com>
2020-03-25 15:38:36 -0300

    Creating IncidentController

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,15 @@
-import crypto from 'crypto';
 import { Router } from 'express';
 
+import IncidentController from './controllers/IncidentController';
 import OngController from './controllers/OngController';
 
 const routes = new Router();
 
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.store);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.store);
+routes.delete('/incidents/:id', IncidentController.delete);
 
 export default routes;
