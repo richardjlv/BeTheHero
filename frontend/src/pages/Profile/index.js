2a6c1eec5b9a07a55017044103e91f79e42a28b9
richardjlv <richardjunio39@gmail.com>
2020-03-25 20:30:34 -0300

    Adding pages

---
+++ frontend/src/pages/Profile/index.js
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from 'react';
+import { FiPower, FiTrash2 } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+
+import './styles.css';
+import logoImg from '../../assets/logo.svg';
+import api from '../../services/api';
+
+export default function Profile() {
+  const [incidents, setIncidents] = useState([]);
+  const ongId = localStorage.getItem('ongId');
+  const ongName = localStorage.getItem('ongName');
+
+  useEffect(() => {
+    async function loadIncidents() {
+      const response = await api.get('incidents', {
+        headers: {
+          Authorization: ongId,
+        },
+      });
+
+      setIncidents(response.data);
+    }
+
+    loadIncidents();
+  }, [ongId]);
+
+  return (
+    <div className="profile-container">
+      <header>
+        <img src={logoImg} alt="Be The Hero" />
+        <span>Bem vinda, {ongName}</span>
+
+        <Link className="button" to="/incidents/new">
+          Cadastrar novo caso
+        </Link>
+        <button type="button">
+          <FiPower size={18} color="#e02041" />
+        </button>
+      </header>
+
+      <h1>Casos cadastrados</h1>
+
+      <ul>
+        {incidents.map((incident) => (
+          <li key={incident.id}>
+            <strong>CASO:</strong>
+            <p>{incident.title}</p>
+
+            <strong>DESCRIÇÃO:</strong>
+            <p>{incident.description}</p>
+
+            <strong>VALOR:</strong>
+            <p>
+              {Intl.NumberFormat('pt-BR', {
+                style: 'currency',
+                currency: 'BRL',
+              }).format(incident.value)}
+            </p>
+
+            <button type="button">
+              <FiTrash2 size={20} color="#a8b8b3" />
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
