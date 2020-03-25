219142e46478fd2bdc59b4fcff6748ef0b46cbde
richardjlv <richardjunio39@gmail.com>
2020-03-25 18:12:41 -0300

    Adding Logon page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import './global.css';
+import Routes from './routes';
 
 export default function App() {
-  return <div>Hello</div>;
+  return <Routes />;
 }
