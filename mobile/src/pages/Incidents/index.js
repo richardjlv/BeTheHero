089901b84ffbcbbc74947ab1dbd1f1f2d66e18bb
richardjlv <richardjunio39@gmail.com>
2020-03-26 21:34:19 -0300

    Adding Incidents page

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,10 +1,58 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Image, Text, FlatList, TouchableOpacity } from 'react-native';
+import { Feather } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native';
 
-// import { Container } from './styles';
+import logoImg from '../../assets/logo.png';
+import styles from './styles';
 
 export default function Incidents() {
+  const navigation = useNavigation();
+
+  function navigateToDetails() {
+    navigation.navigate('Detail');
+  }
+
   return (
-    <View />
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={logoImg} />
+        <Text style={styles.headerText}>
+          Total de <Text style={styles.headerTextBold}>0 casos</Text>
+        </Text>
+      </View>
+
+      <Text style={styles.title}>Bem-vindo!</Text>
+      <Text style={styles.description}>Escolha um dois casos abaixo e salve o dia</Text>
+
+      <FlatList
+        style={styles.incidentList}
+        data={[1, 2, 3]}
+        keyExtractor={incident => String(incident)}
+        showsVerticalScrollIndicator={false}
+        renderItem={() => (
+          <View style={styles.incident}>
+            <Text style={styles.incidentProperty}>ONG:</Text>
+            <Text style={styles.incidentValue}>APAD</Text>
+
+            <Text style={styles.incidentProperty}>Caso:</Text>
+            <Text style={styles.incidentValue}>Cadelinha atropelada</Text>
+
+            <Text style={styles.incidentProperty}>VALOR:</Text>
+            <Text style={styles.incidentValue}>R$ 120,00</Text>
+
+            <TouchableOpacity
+              style={styles.detailsButton}
+              onPress={navigateToDetails}
+            >
+              <Text style={styles.detailsButtonText}>
+                Ver mais detalhes
+            </Text>
+              <Feather name='arrow-right' size={16} color='#e02041' />
+            </TouchableOpacity>
+          </View>
+        )}
+      />
+    </View>
   );
 }
